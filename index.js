25f589f906232dd1f84f809d4fdb1d8f40857ae0
patriciogarnero <80570463+patriciogarnero@users.noreply.github.com>
2022-05-06 15:58:07 +0200

    removed logs

--- index.js
+++ index.js
@@ -5,10 +5,8 @@ menu.style.display = "none";
 
 menuIcon.addEventListener("click", () => {
   if (menu.style.display === "none") {
-    console.log("click");
     menu.style.display = "flex";
   } else if (menu.style.display === "flex") {
-    console.log("other click");
     menu.style.display = "none";
   }
 });
