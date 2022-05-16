07638518f4ff219f7dc04d05765a72ddd8db956b
patriciogarnero <80570463+patriciogarnero@users.noreply.github.com>
2022-05-16 14:33:46 +0200

    working on desktop layout

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const menuIcon = document.querySelector(".menu-icon");
 const menu = document.querySelector(".menu");
 
-menu.style.display = "none";
-
 menuIcon.addEventListener("click", () => {
-  if (menu.style.display === "none") {
+  if (getComputedStyle(menu).display === "none") {
     menu.style.display = "flex";
-  } else if (menu.style.display === "flex") {
+  } else {
     menu.style.display = "none";
   }
 });
