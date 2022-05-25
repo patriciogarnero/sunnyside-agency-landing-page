cc8d5c90a975fa6534e290634888f67150b53e37
patriciogarnero <80570463+patriciogarnero@users.noreply.github.com>
2022-05-25 10:22:13 +0200

    finished mobile menu

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const menu = document.querySelector(".menu");
 menuIcon.addEventListener("click", () => {
   if (getComputedStyle(menu).display === "none") {
     menu.style.display = "flex";
+    menuIcon.style.opacity = "0.6";
   } else {
     menu.style.display = "none";
+    menuIcon.style.opacity = "1";
   }
 });
